9f9e061334dbcf346067eff4d214117feaff2fb1
Robert Oware <boyr47@gmail.com>
2023-02-19 06:30:17 -0800

    Prints number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints single digits 
+ * base 10 from 0
+ * Return: Zero if success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
