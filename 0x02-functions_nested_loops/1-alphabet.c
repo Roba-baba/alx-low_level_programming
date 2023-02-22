84fe529caf882f27512c23bd6667a3113c64e0fa
Robert Oware <boyr47@gmail.com>
2023-02-22 03:44:30 -0800

    prints in alphabet lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * Prints alphabet in lowercase and adds a new line
+ *
+ */
+void print_alphabet(void);
+{
+	char (n);
+	for (n = 'a'; n = 'z'; a++)
+	{
+		_putchar (n);
+	}
+	_putchar ('\n');
+}
+
