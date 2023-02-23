9f5fbe1b12f5038139fc1acfb1c8124fada571e0
Robert Oware <boyr47@gmail.com>
2023-02-23 06:11:21 -0800

    main as a header

--- 0-putchar.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int _putchar(char _putchar)
-{
-	return(0);
-}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef main
+#define main
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* main.h */
