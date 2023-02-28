f889733a25eec574836fb02e20231070a35218f9
Robert Oware <boyr47@gmail.com>
2023-02-28 07:37:38 -0800

    updates number

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,22 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of n to 98
- * @n: integer pointer to a variable to be updated
- *
- * Return: nothing
+ * reset_to_98 - function declaration
+ * @n: pointer integer
+ * Return: void
+ */
+
+void reset_to_98(int *n);
+
+/**
+ * reset_to_98 - function definition
+ * Description: resets value to 98
+ * @n: the pointer integer
+ * Return: void
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
